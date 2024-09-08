a5e6214f1ccbdef61c7734c590a3d22c1dacdfec
陈奇 <chenqi2257@foryou56.com>
2024-09-08 11:05:45 +0800

    robot commit

---
+++ mp-weixin/components/uni-ui/lib/uni-icons/uni-icons.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["components/uni-ui/lib/uni-icons/uni-icons"],{"01f2":function(n,t,e){"use strict";e.r(t);var u=e("a71e"),f=e.n(u);for(var i in u)"default"!==i&&function(n){e.d(t,n,(function(){return u[n]}))}(i);t["default"]=f.a},"55bf":function(n,t,e){"use strict";var u;e.d(t,"b",(function(){return f})),e.d(t,"c",(function(){return i})),e.d(t,"a",(function(){return u}));var f=function(){var n=this,t=n.$createElement;n._self._c},i=[]},6286:function(n,t,e){"use strict";e.r(t);var u=e("55bf"),f=e("01f2");for(var i in f)"default"!==i&&function(n){e.d(t,n,(function(){return f[n]}))}(i);e("7dff");var r,c=e("f0c5"),a=Object(c["a"])(f["default"],u["b"],u["c"],!1,null,"5b2edd92",null,!1,u["a"],r);t["default"]=a.exports},"7dff":function(n,t,e){"use strict";var u=e("efe7"),f=e.n(u);f.a},a71e:function(n,t,e){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var u=f(e("0336"));function f(n){return n&&n.__esModule?n:{default:n}}var i={name:"UniIcons",props:{type:{type:String,default:""},color:{type:String,default:"#333333"},size:{type:[Number,String],default:16}},data:function(){return{icons:u.default}},methods:{_onClick:function(){this.$emit("click")}}};t.default=i},efe7:function(n,t,e){}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/uni-ui/lib/uni-icons/uni-icons-create-component',
+    {
+        'components/uni-ui/lib/uni-icons/uni-icons-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('543d')['createComponent'](__webpack_require__("6286"))
+        })
+    },
+    [['components/uni-ui/lib/uni-icons/uni-icons-create-component']]
+]);
